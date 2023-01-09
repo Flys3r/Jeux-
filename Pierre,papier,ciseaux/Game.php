d566e6a6035c39cfd55043501629f386a180e19a
nitnel <118741473+nitnel@users.noreply.github.com>
2023-01-09 10:56:28 +0100

    Update Game.php

--- Pierre,papier,ciseaux/Game.php
+++ Pierre,papier,ciseaux/Game.php
@@ -72,10 +72,6 @@ echo '</div>';
 
 
 ?>
-
-
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -90,21 +86,29 @@ echo '</div>';
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <div class="row g-0 bg-body-secondary position-relative">
       <div class="col-md-6 mb-md-0 p-md-4">
-      <img src="..." class="w-100" alt="...">
+        <form>
+    
+        </form>
+        
+       <img src="..." class="w-100" alt="...">
     </div>
+
     <div class="card" style="width: 18rem;">
      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Ffr%2Fphotos%2Fshifumi&psig=AOvVaw2H7eNNAJPQi48h7pOkMRSC&ust=1673340888253000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJDBs9uOuvwCFQAAAAAdAAAAABAE" class="card-img-top" alt="...">
     <div class="card-body">
       <a href="#" class="stretched-link text-danger" style="position: relative;"> 
-    </p>
-    <p class="card-text bg-light" style="transform: rotate(0);">
-     
-    </p>
+     <p class="card-text bg-light" style="transform: rotate(0);">
+     </p>
     </div>
     </div>
   </body>
 
 </html>
+
+
+
+
+
 
   
   
