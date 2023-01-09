1e0914adb8a0af5a6734b547280ebc6f323358ab
chaoiil <118742832+chaoiil@users.noreply.github.com>
2023-01-09 10:58:15 +0100

    Update Home.php

---
+++ Pierre,papier,ciseaux/Home.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>   
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script> 
+    <link rel="stylesheet" href="./stylee.css">
+    <title>pierre feuille ciseaux</title>
+  </head>
+  <body>
+  <h1>BIENVENUE</h1>
+  <img src="./image/shifumi.jpg" alt="shifumi">
+    <div>
+    <div class="accordion" id="accordionExample">
+    <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        REGLE DE JEU
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>REGLE DE JEU.</strong>Pierre-feuille-ciseaux est un jeu qui se joue avec les mains, et à deux joueurs. Chacun des joueurs met une main dans le dos et va choisir un des coups possibles (pierre/feuille ou ciseaux), et le symboliser avec sa main, toujours dans son dos. Pour le coup "pierre", le joueur ferme le poing. Pour le coup "feuille", il met sa main à plat. Et pour le dernier coup possible, le "ciseau", le joueur écarte ses deux doigts. Les deux joueurs révèlent ensuite leur main, et donc, le coup qu’ils décident de jouer, simultanément. De façon générale : - Le jeux de "pierre" bat les ciseaux (en les écrasant).
+        - Le jeux de "ciseaux" bat la feuille (en la coupant).
+        - La "feuille" bat la pierre (en l'enveloppant). 
+        Ainsi, chaque coup bat un autre coup. 
+       Si les joueurs révèlent le même symbole (pierre/pierre, feuille/feuille, ciseaux/ciseaux), cela fait match nul : personne ne marque de points..
+      </div>
+    </div>
+    </div>
+    </div>
+    <div class="d-grid gap-2">
+   <button class="btn btn-primary" type="button">Cliquez-ici</button>
+   </div>
+  </body>
+</html>
