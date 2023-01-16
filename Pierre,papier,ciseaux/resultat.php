b44f4b5b3302ab47e9ce57ac6517a887e35cffe3
Flys3r <118739922+Flys3r@users.noreply.github.com>
2023-01-16 09:47:03 +0100

    Update resultat.php

---
+++ Pierre,papier,ciseaux/resultat.php
@@ -0,0 +1,17 @@
+$options = [
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"];
+$dbh = new PDO('mysql:host=localhost;dbname=shifumi','root', '',$options);
+
+$sth = $dbh->prepare("SELECT * FROM utilisateur");  
+$sth->execute();
+$shifumi = $sth->fetchAll();
+
+foreach($groupes as $groupe){
+    echo "<p>",$shifumi ['nom'],"</p>";
+    echo "<p>",$shifumi ['date'],"</p>";
+    echo "<p>",$shifumi ['nbr_victoire'],"</p>";
+    echo "<p>",$shifumi ['nbr_partie'],"</p>";
+    echo "<p>",$shifumi ['egalite'],"</p>";
+    echo "<img src='",$shifumi ['image'],"'/>";
+    
+}
