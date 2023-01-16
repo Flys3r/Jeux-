ceec684bafb6eb0555512f070927fda34a3597d7
nitnel <118741473+nitnel@users.noreply.github.com>
2023-01-16 10:39:22 +0100

    Update Home.php

--- Pierre,papier,ciseaux/Home.php
+++ Pierre,papier,ciseaux/Home.php
@@ -20,10 +20,10 @@
     <div class="collapse navbar-collapse" id="navbarScroll">
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">tableau de score</a>
+          <a class="nav-link active" aria-current="page" href="#">Tableau de score</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">jouer</a>
+          <a class="nav-link active" aria-current="page" href="#">Jouer</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
